d1cc5bbc0f62634ca241040c399885eb25c8751c
TeukuNopri <teukunopriansyah3@gmail.com>
2023-02-11 09:45:24 +0700

    update
    login nya bg

--- app/src/main/java/com/tamiang/smart/LoginActivity.kt
+++ app/src/main/java/com/tamiang/smart/LoginActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.tamiang.smart.models.LoginRequest
 import com.tamiang.smart.models.LoginResponse
 import com.tamiang.smart.network.NetworkConfig
 import com.tamiang.smart.sharepreflogin.SharedPrefManager
@@ -32,13 +33,32 @@ class LoginActivity : AppCompatActivity() {
         val token = sharedPreference?.getString("token", "").toString()
 
         btn_action_login.setOnClickListener {
-            val rekam_medik = et_login_medik.text.toString().trim()
-            val nik         = et_login_nik.text.toString().trim()
-            val tgl_lahir   = et_login_tgl_lahir.text.toString().trim()
-            val token       = token
-            val device      = Build.MANUFACTURER.toString()
+//            val rekam_medik = et_login_medik.text.toString().trim()
+//            val nik         = et_login_nik.text.toString().trim()
+//            val tgl_lahir   = et_login_tgl_lahir.text.toString().trim()
+//            val token       = token
+//            val device      = Build.MANUFACTURER.toString()
 
-            checkLogin(rekam_medik, nik, tgl_lahir, token, device)
+            val loginRequest = LoginRequest(
+                "255388",
+                "111",
+                "2006-11-23"
+            )
+
+            Log.d("s", loginRequest.toString())
+
+            pb_login.visibility = View.VISIBLE
+            NetworkConfig.getInstance().cekLogin(loginRequest).enqueue(object : Callback<LoginResponse>{
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                    Log.d("Data", response.body()?.metaData.toString())
+                }
+
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Log.e("Network Error", t.message.toString())
+                }
+
+            })
+//            checkLogin(rekam_medik, nik, tgl_lahir, token, device)
         }
 
         et_login_tgl_lahir.setOnClickListener {
@@ -58,40 +78,40 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    private fun checkLogin(no_rkm_medik: String?, nik: String?, tgl_lahir: String?, token: String?, device: String?){
-        pb_login.visibility = View.VISIBLE
-        NetworkConfig.getInstance().cekLogin(no_rkm_medik, nik, tgl_lahir, token, device)
-            .enqueue(object: Callback<LoginResponse>{
-                override fun onResponse(
-                    call: Call<LoginResponse>,
-                    response: Response<LoginResponse>
-                ) {
-
-                    pb_login.visibility = View.GONE
-
-                    if(response.body()?.metaData?.kode == 202){
-                        Toast.makeText(applicationContext, response.body()?.metaData?.message.toString(), Toast.LENGTH_LONG).show()
-                    }else if(response.body()?.metaData?.kode == 201){
-                        Toast.makeText(applicationContext, response.body()?.metaData?.message.toString(), Toast.LENGTH_LONG).show()
-                    }else{
-                        SharedPrefManager.getInstance(application).saveUser(response.body()?.response!!)
-
-                        val intent = Intent(applicationContext, HomeActivity::class.java)
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-
-                        startActivity(intent)
-
-                        et_login_medik.text.clear()
-                        et_login_nik.text.clear()
-                        et_login_tgl_lahir.text.clear()
-
-                        finish()
-                    }
-                }
-
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    Log.e("Network Error", t.message.toString())
-                }
-            })
-    }
+//    private fun checkLogin(no_rkm_medik: String?, nik: String?, tgl_lahir: String?, token: String?, device: String?){
+//        pb_login.visibility = View.VISIBLE
+//        NetworkConfig.getInstance().cekLogin(no_rkm_medik, nik, tgl_lahir, token, device)
+//            .enqueue(object: Callback<LoginResponse>{
+//                override fun onResponse(
+//                    call: Call<LoginResponse>,
+//                    response: Response<LoginResponse>
+//                ) {
+//
+//                    pb_login.visibility = View.GONE
+//
+//                    if(response.body()?.metaData?.kode == 202){
+//                        Toast.makeText(applicationContext, response.body()?.metaData?.message.toString(), Toast.LENGTH_LONG).show()
+//                    }else if(response.body()?.metaData?.kode == 201){
+//                        Toast.makeText(applicationContext, response.body()?.metaData?.message.toString(), Toast.LENGTH_LONG).show()
+//                    }else{
+//                        SharedPrefManager.getInstance(application).saveUser(response.body()?.response!!)
+//
+//                        val intent = Intent(applicationContext, HomeActivity::class.java)
+//                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//
+//                        startActivity(intent)
+//
+//                        et_login_medik.text.clear()
+//                        et_login_nik.text.clear()
+//                        et_login_tgl_lahir.text.clear()
+//
+//                        finish()
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+//                    Log.e("Network Error", t.message.toString())
+//                }
+//            })
+//    }
 }
